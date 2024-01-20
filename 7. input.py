a89f19f488da2dc795af04024d6744ebeaa8b3e0
Avivmalik <Avivmalik77@gmail.com>
2024-01-20 20:03:02 +0700

    hari 3

---
+++ 7. input.py
@@ -0,0 +1,15 @@
+# Input user
+
+# data yang masuk pasti string
+
+# data = input("masukan data: ")
+# print("data = ",data,",type=",type(data))
+
+# # jika ingin mengambil int, maka
+# angka = int(input("masukan angka: "))
+# angka = float(input("masukan angka: "))
+# print("data= ",data_int,", type=",type(angka))
+
+# untuk bolean perlu di casting ke integer
+biner = bool(int(input("masukan nilai bololean: ")))
+print("data = ",biner,"<type +",type(biner))
